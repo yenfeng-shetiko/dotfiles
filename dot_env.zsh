39193a18479c7bd52173af03fe856f230844f37c
loke <lok-ation@outlook.com>
2023-08-03 08:39:19 +0800

    add useful alias form hightemp's .bash_alias

--- dot_env.zsh
+++ dot_env.zsh
@@ -9,6 +9,28 @@ export BAT_STYLE=plain
 export BAT_THEME="TwoDark"
 export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 export MANROFFOPT="-c"
+# Clipboard
+{{ if eq .chezmoi.os "linux"}}alias getcb='wl-paste'{{ end }}
+{{ if eq .chezmoi.os "windows"}}alias getcb='cat /dev/clipboard'{{ end }}
+{{ if eq .chezmoi.os "android"}}alias getcb='termux-clipboard-get'{{ end }}
+
+# Wget
+alias wgetncc='wget --no-check-certificate'
+alias wgetc='wget `getcb`'
+
+function wget_archive_and_extract {
+  URL=$1
+  FILENAME=${URL##*/}
+  wget $URL -O $FILENAME
+  extract -r $FILENAME
+}
+alias wgetae='wget_archive_and_extract '
+alias wgetaec='wgetae getcb'
+
+# Utils
+alias sitecopy='wget -k -K -E -r -l 10 -p -N -F -nH '
+alias ytmp3='youtube-dl --extract-audio --audio-format mp3 '
+# rga
 rga-fzf() {
 	RG_PREFIX="rga --files-with-matches"
 	local file
