66302a9c7963f8b157870da0da7242a59fd092ca
loke <lok-ation@outlook.com>
2023-09-15 21:33:22 +0800

    Improve runlike alias

--- dot_renv.zsh
+++ dot_renv.zsh
@@ -1,2 +1,2 @@
-alias runlike="docker run --rm -v /var/run/docker.sock:/var/run/docker.sock assaflavie/runlike"
+alias runlike="docker pull assaflavie/runlike && docker image prune && docker run --rm -v /var/run/docker.sock:/var/run/docker.sock assaflavie/runlike"
 . "/root/.acme.sh/acme.sh.env"
