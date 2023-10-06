a4c86c5d135c61569d83c681cbe945bff5008f5e
loke <lok-ation@outlook.com>
2023-10-06 11:32:06 +0800

    Removing pacaptr's alias

--- dot_renv.zsh
+++ dot_renv.zsh
@@ -1,3 +1,2 @@
 alias runlike="docker pull -q assaflavie/runlike && docker image prune -f && docker run --rm -v /var/run/docker.sock:/var/run/docker.sock assaflavie/runlike"
-alias pacman=pacaptr
 . "/root/.acme.sh/acme.sh.env"
