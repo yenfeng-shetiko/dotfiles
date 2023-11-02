e56ed7a93db8b1862ded5c9f0d1a344c1be8560c
loke <lok-ation@outlook.com>
2023-11-02 15:07:07 +0800

    Modify doom config

--- dot_config/doom/init.el
+++ dot_config/doom/init.el
@@ -21,11 +21,10 @@
        ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
-       vertico           ; the ultimate code completion backend
+       (vertico +icons)  ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ;;ivy               ; a search engine for love and life
-       vertico           ; the search engine of the future
 
        :ui
        ;;deft              ; notational velocity for Emacs
