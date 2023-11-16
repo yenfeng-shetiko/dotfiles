1562613eaebbe437377140517a3cc6ceea50e8f0
loke <lok-ation@outlook.com>
2023-11-16 23:40:17 +0800

    remove renv.zsh

--- dot_renv.zsh
+++
@@ -1,2 +0,0 @@
-alias runlike="docker pull -q assaflavie/runlike && docker image prune -f && docker run --rm -v /var/run/docker.sock:/var/run/docker.sock assaflavie/runlike"
-. "/root/.acme.sh/acme.sh.env"
